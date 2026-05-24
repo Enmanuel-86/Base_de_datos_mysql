6403b314123f62ff3448a074dd646e45dad27148
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-05-24 11:38:20 -0400

    prueba de la consulta

--- scripts_sql/consultas_rentmasterengine.sql
+++ scripts_sql/consultas_rentmasterengine.sql
@@ -3,6 +3,13 @@ SELECT * FROM rentmasterengine.equipos;
 SELECT * FROM rentmasterengine.alquileres;
 SELECT * FROM rentmasterengine.auditorias;
 
+SELECT * FROM rentmasterengine.alquileres
+WHERE id_cliente = 1;
+CALL rentmasterengine.sp_cancelar_alquileres_por_cliente_id_y_monto(1, 2710, @res );
+SELECT @res;
+
+CALL rentmasterengine.sp_registrar_alquiler_por_id(1,3, @respuesta);
+SELECT @respuesta;
 
 SELECT * FROM rentmasterengine.vw_alquileres_de_equipos;
 SELECT * FROM rentmasterengine.vw_auditoria_de_equipos;
@@ -15,9 +22,11 @@ SELECT * FROM rentmasterengine.vw_equipos_disponibles;
 SELECT * FROM rentmasterengine.vw_equipos_en_mantenimiento;
 
 
+
+
 START TRANSACTION;
 
-CALL rentmasterengine.sp_pagar_alquiler_por_id_alquiler_y_monto(16, 620.00, @respuesta);
+CALL rentmasterengine.sp_pagar_alquiler_por_id_alquiler_y_monto(4, 800, @respuesta);
 SELECT @respuesta;
 
 ROLLBACK;
