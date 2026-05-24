eea9c07d3c7560e8dfcf3f0b7024f270669f7b8d
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-05-24 10:41:08 -0400

    solo se renombraron

--- scripts_sql/consultas_rentmasterengine.sql
+++ scripts_sql/consultas_rentmasterengine.sql
@@ -1,7 +1,26 @@
 SELECT * FROM rentmasterengine.clientes;
 SELECT * FROM rentmasterengine.equipos;
 SELECT * FROM rentmasterengine.alquileres;
+SELECT * FROM rentmasterengine.auditorias;
 
-CALL rentmasterengine.sp_registrar_alquiler_por_id(1)
 
 SELECT * FROM rentmasterengine.vw_alquileres_de_equipos;
+SELECT * FROM rentmasterengine.vw_auditoria_de_equipos;
+SELECT * FROM rentmasterengine.vw_equipos_alquilados_y_no_alquilados;
+SELECT * FROM rentmasterengine.vw_equipos_auditados_y_no_auditados;
+
+# Equipos segun su estado
+SELECT * FROM rentmasterengine.vw_equipos_alquilados;
+SELECT * FROM rentmasterengine.vw_equipos_disponibles;
+SELECT * FROM rentmasterengine.vw_equipos_en_mantenimiento;
+
+
+START TRANSACTION;
+
+CALL rentmasterengine.sp_pagar_alquiler_por_id_alquiler_y_monto(16, 620.00, @respuesta);
+SELECT @respuesta;
+
+ROLLBACK;
+COMMIT;
+
+
