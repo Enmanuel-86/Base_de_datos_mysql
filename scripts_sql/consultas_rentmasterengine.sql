78f68dff5b21b92fe8246dee4622653f6b00dafb
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-05-25 05:32:13 -0400

    bosquejo de la nueva view del commit anterior

--- scripts_sql/consultas_rentmasterengine.sql
+++ scripts_sql/consultas_rentmasterengine.sql
@@ -3,13 +3,6 @@ SELECT * FROM rentmasterengine.equipos;
 SELECT * FROM rentmasterengine.alquileres;
 SELECT * FROM rentmasterengine.auditorias;
 
-SELECT * FROM rentmasterengine.alquileres
-WHERE id_cliente = 1;
-CALL rentmasterengine.sp_cancelar_alquileres_por_cliente_id_y_monto(1, 2710, @res );
-SELECT @res;
-
-CALL rentmasterengine.sp_registrar_alquiler_por_id(1,3, @respuesta);
-SELECT @respuesta;
 
 SELECT * FROM rentmasterengine.vw_alquileres_de_equipos;
 SELECT * FROM rentmasterengine.vw_auditoria_de_equipos;
@@ -21,15 +14,24 @@ SELECT * FROM rentmasterengine.vw_equipos_alquilados;
 SELECT * FROM rentmasterengine.vw_equipos_disponibles;
 SELECT * FROM rentmasterengine.vw_equipos_en_mantenimiento;
 
+SELECT  c.nombre , COUNT(a.id_cliente) AS Cant_alquileres,
+		SUM(a.total_pagar) AS Total
+FROM rentmasterengine.alquileres AS a 
+INNER JOIN rentmasterengine.clientes AS c
+	ON c.id_cliente = a.id_cliente
+WHERE a.estado_pago = 'NO PAGADO'
+GROUP BY c.nombre
+ORDER BY total DESC;
+
+call rentmasterengine.sp_registrar_alquiler_por_id(7,3, @res);
+SELECT @res;
 
 
+CALL rentmasterengine.sp_pagar_alquiler_por_id_alquiler_y_monto(19,1100, @res);
+SELECT @res;
 
-START TRANSACTION;
 
-CALL rentmasterengine.sp_pagar_alquiler_por_id_alquiler_y_monto(4, 800, @respuesta);
-SELECT @respuesta;
-
-ROLLBACK;
-COMMIT;
+CALL rentmasterengine.sp_cancelar_alquileres_por_cliente_id_y_monto(7, 3200, @res);
+SELECT @res;
 
 
