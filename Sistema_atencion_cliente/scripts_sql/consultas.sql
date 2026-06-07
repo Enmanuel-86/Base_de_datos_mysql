0b26908aa2d27bec319d14fcdb7f5259407429a2
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-06-07 19:58:38 -0400

    mas pruebas

--- Sistema_atencion_cliente/scripts_sql/consultas.sql
+++ Sistema_atencion_cliente/scripts_sql/consultas.sql
@@ -1,4 +1,17 @@
 SELECT * FROM sistema_atencion_clientes.clientes;
+SELECT * FROM sistema_atencion_clientes.empleados;
+
+SELECT * FROM sistema_atencion_clientes.en_espera_cliente;
+SELECT * FROM sistema_atencion_clientes.atencion_al_cliente;
 
 CALL sistema_atencion_clientes.sp_registrar_cliente('Enmanuel', 'Garcia', 'enmanue@gmail.com', @respuesta);
 SELECT @respuesta;
+
+CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(1, @respuesta);
+
+
+TRUNCATE sistema_atencion_clientes.clientes;
+TRUNCATE sistema_atencion_clientes.empleados;
+
+INSERT INTO sistema_atencion_clientes.atencion_al_cliente (id_empleado, id_cliente, hora_comienzo, fecha_atencion)
+VALUES (1, 1,CURRENT_TIME(), CURRENT_DATE());
