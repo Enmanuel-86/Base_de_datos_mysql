cf46aff8184e8c390cebd68ce96dbe49dc53c623
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-06-21 17:44:07 -0400

    falta estructurar consultas basicas y crear views

--- Sistema_atencion_cliente/scripts_sql/consultas.sql
+++ Sistema_atencion_cliente/scripts_sql/consultas.sql
@@ -3,22 +3,23 @@ SELECT * FROM sistema_atencion_clientes.empleados;
 
 SELECT * FROM sistema_atencion_clientes.en_espera_cliente;
 SELECT * FROM sistema_atencion_clientes.atencion_al_cliente;
+SELECT * FROM sistema_atencion_clientes.auditoria;
 
+CALL sistema_atencion_clientes.sp_finalizar_atencion_cliente(1, @res);
+SELECT @res;
 CALL sistema_atencion_clientes.sp_registrar_cliente('Enmanuel', 'Garcia', 'enmanue@gmail.com', @respuesta);
 SELECT @respuesta;
 
 CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(1, @respuesta);
+CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(2, @respuesta);
+CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(3, @respuesta);
+CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(4, @respuesta);
+CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(5, @respuesta);
 
 
 TRUNCATE sistema_atencion_clientes.clientes;
 TRUNCATE sistema_atencion_clientes.empleados;
 TRUNCATE sistema_atencion_clientes.en_espera_cliente;
 TRUNCATE sistema_atencion_clientes.atencion_al_cliente;
+TRUNCATE sistema_atencion_clientes.auditoria;
 
-
-SELECT TIMEDIFF(CURRENT_TIME(), '05:10:00') as duracion;
-
-
-
-CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(1, @respuesta);
-CALL sistema_atencion_clientes.sp_registrar_atencion_cliente_por_id(1, 1, @respuesta);
