9215008f058d3866f38874a2a20f98f4651b066e
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-06-22 05:25:26 -0400

    Se organizaron las consultas para que las personas que prueben esta base de datos la puedan utilizar facil y rapido
    se colocaron:
    	- Consultas de las tablas
    	- Vista de las tablas con sus inner joins
    	- Uso de los Stored Procedures de las operacion de la base de datos
    	- Uso de los Truncates para reiniciar las tablas

--- Sistema_atencion_cliente/scripts_sql/consultas.sql
+++ Sistema_atencion_cliente/scripts_sql/consultas.sql
@@ -1,25 +1,39 @@
+-- Consulta de las tablas
 SELECT * FROM sistema_atencion_clientes.clientes;
 SELECT * FROM sistema_atencion_clientes.empleados;
-
 SELECT * FROM sistema_atencion_clientes.en_espera_cliente;
 SELECT * FROM sistema_atencion_clientes.atencion_al_cliente;
 SELECT * FROM sistema_atencion_clientes.auditoria;
 
-CALL sistema_atencion_clientes.sp_finalizar_atencion_cliente(1, @res);
-SELECT @res;
-CALL sistema_atencion_clientes.sp_registrar_cliente('Enmanuel', 'Garcia', 'enmanue@gmail.com', @respuesta);
-SELECT @respuesta;
+-- Consultas con vistas
+SELECT * FROM sistema_atencion_clientes.vw_atencion_al_cliente;
+SELECT * FROM sistema_atencion_clientes.vw_atencion_al_cliente_resumen;
+SELECT * FROM sistema_atencion_clientes.vw_clientes_en_espera;
+SELECT * FROM sistema_atencion_clientes.vw_auditoria;
 
-CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(1, @respuesta);
-CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(2, @respuesta);
-CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(3, @respuesta);
-CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(4, @respuesta);
-CALL sistema_atencion_clientes.sp_registrar_espera_cliente_por_id(5, @respuesta);
-
-
+-- Reiniciar tablas
 TRUNCATE sistema_atencion_clientes.clientes;
 TRUNCATE sistema_atencion_clientes.empleados;
 TRUNCATE sistema_atencion_clientes.en_espera_cliente;
 TRUNCATE sistema_atencion_clientes.atencion_al_cliente;
 TRUNCATE sistema_atencion_clientes.auditoria;
 
+-- Operaciones del sistema
+
+-- 1. Registrar el cliente en la lista de espera o en cola (id_cliente)
+CALL sistema_atencion_clientes.sp_registrar_espera_con_cliente_id(1, @res);
+CALL sistema_atencion_clientes.sp_registrar_espera_con_cliente_id(2, @res);
+CALL sistema_atencion_clientes.sp_registrar_espera_con_cliente_id(3, @res);
+CALL sistema_atencion_clientes.sp_registrar_espera_con_cliente_id(4, @res);
+CALL sistema_atencion_clientes.sp_registrar_espera_con_cliente_id(5, @res);
+SELECT @res;
+
+-- 2. Atender al cliente (id_cliente, id_empleado)
+CALL sistema_atencion_clientes.sp_registrar_atencion_con_cliente_y_empleado_id(1, 2, @res);
+CALL sistema_atencion_clientes.sp_registrar_atencion_con_cliente_y_empleado_id(2, 1, @res);
+SELECT @res;
+
+-- 3. Finalizar la atencion al cliente y registrar la auditoria (id_cliente)
+CALL sistema_atencion_clientes.sp_finalizar_atencion_con_cliente_id(1, @res);
+CALL sistema_atencion_clientes.sp_finalizar_atencion_con_cliente_id(2, @res);
+SELECT @res;
